9c374600135f3112998f37eeb0b42afe9003a699
giovanalari <158284410+giovanalari@users.noreply.github.com>
2024-02-18 23:11:30 -0300

    script.js

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ var mensagemGlobal;
 function conferirMensagemWhatsApp(){
     var nome = document.getElementById("nome").value;
     var mensagem = document.getElementById("mensagem").value;
+    
     nomeGlobal = nome;
     mensagemGlobal = mensagem;
 
-
-    document.getElementById("cofnome").textContent = nome;
-    document.getElementById("cofmsg").textContent = mensagem;
+    document.getElementById("confnome").textContent = nome;
+    document.getElementById("confmsg").textContent = mensagem;
 }
 
-    function enviarzap(){
-     var numerotelefone = "5541996462110"
+function enviarzap(){
+    var numerotelefone = "5541920004260"
     
-     var linkwhatsApp = "https://wa.me/" +
-     numerotelefone +
-      "?text=nome:" +
-       nomeGlobal +
-        "-" +
-     mensagemGlobal;
+    var linkwhatsApp = "https://wa.me/" + 
+    numerotelefone + 
+    "?text=Nome:" + 
+    nomeGlobal + 
+    " - " + 
+    mensagemGlobal;
     window.open(linkwhatsApp, "_blank")
 }
